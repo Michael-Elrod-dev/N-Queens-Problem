64d6e7d9a2ef884f066159a296b80f39d2a817f4
Michael Elrod <117390129+Michael-Elrod-dev@users.noreply.github.com>
2023-03-16 10:34:27 -0400

    Update nQueens.cpp

--- nQueens.cpp
+++ nQueens.cpp
@@ -1,89 +1,62 @@
-/*
- * Name: Michael Elrod
- * Date Submitted: 11/15/2022
- * Lab Section: 001
- * Assignment Name: Lab 9: Using Recursion to Solve the N-Queens Problem
- */
-
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-// Checks surrounding positions to see if a Queen can be placed
-bool queenCanBePlaced(int** queens, int n, int col, int row)
-{
+// Checks if a queen can be placed at the given row and column.
+bool queenCanBePlaced(int** queens, int n, int col, int row) {
     // Check the row
-    for(int c=0;c<n;c++)
-        if(queens[row][c] == 1)
+    for (int c = 0; c < n; c++)
+        if (queens[row][c] == 1)
             return false;
 
     // Check lower diagonal (left to right)
-    for(int r=row,c=col;r>=0 and c>=0;r--,c--)
-        if(queens[r][c] == 1)
+    for (int r = row, c = col; r >= 0 && c >= 0; r--, c--)
+        if (queens[r][c] == 1)
             return false;
 
     // Check upper diagonal (left to right)
-    for(int r=row,c=col;r<n and c>=0;r++,c--)
-        if(queens[r][c] == 1)
+    for (int r = row, c = col; r < n && c >= 0; r++, c--)
+        if (queens[r][c] == 1)
             return false;
 
     return true;
 }
 
-int countSolutions(int** queens, int n, int col)
-{
+// Returns the number of solutions for the N-Queens problem.
+int countSolutions(int** queens, int n, int col) {
     int count = 0;
 
-    if(n == col)
+    if (n == col)
         return 1;
 
-    for(int i=0;i<n;i++){
-        int row = i;
-        //Check for valid positions, then place the Queens
-        if(queenCanBePlaced(queens, n, col, row)){
-            // Place Queen
+    for (int row = 0; row < n; row++) {
+        if (queenCanBePlaced(queens, n, col, row)) {
             queens[row][col] = 1;
-            // Search again
-            count += countSolutions(queens, n, col+1);
+            count += countSolutions(queens, n, col + 1);
             queens[row][col] = 0;
         }
     }
     return count;
 }
 
-int nQueens(int n)
-{
-    // To pass a 2D array we use double indirection (int**)
-    int** queens = new int *[n];
+int nQueens(int n) {
+    int** queens = new int*[n];
 
-    // Make array 2D
-    for(int i=0;i<n;i++){
+    // Initialize the board
+    for (int i = 0; i < n; i++) {
         queens[i] = new int[n];
-
-        // Fill spaces with '0'
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
             queens[i][j] = 0;
     }
+
     // Count the number of solutions
-    return countSolutions(queens,n,0);
+    return countSolutions(queens, n, 0);
 }
 
-/*
-int main()
-{
-    cout << "1: " << nQueens(1) << endl;
-    cout << "2: " << nQueens(2) << endl;
-    cout << "3: " << nQueens(3) << endl;
-    cout << "4: " << nQueens(4) << endl;
-    cout << "5: " << nQueens(5) << endl;
-    cout << "6: " << nQueens(6) << endl;
-    cout << "7: " << nQueens(7) << endl;
-    cout << "8: " << nQueens(8) << endl;
-    cout << "9: " << nQueens(9) << endl;
-    cout << "10: " << nQueens(10) << endl;
-    cout << "11: " << nQueens(11) << endl;
-    cout << "12: " << nQueens(12) << endl;
+int main() {
+    for (int i = 1; i <= 12; i++) {
+        cout << i << ": " << nQueens(i) << endl;
+    }
     return 0;
 }
-*/
